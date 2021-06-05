820f5ffc625d8fce40de2635422cc4333b791f3c
mandeep <singh.mr.1996@gmail.com>
2021-06-05 09:44:17 +0530

    uc2_Show_Number_Of_Times_Head_and_Tail

--- flipcoinsimulation.sh
+++ flipcoinsimulation.sh
@@ -6,3 +6,18 @@ if [[ $toss -eq 0 ]]; then
 else
 	echo Tail Wins
 fi
+heads=0
+tails=0
+for (( i = 0; i < 20; i++ )); do
+	toss=$((RANDOM%2))
+
+	if [[ $toss -eq 0 ]]; then
+		((++heads))
+	else
+		((++tails))
+	fi
+
+done
+
+echo Head Won $heads times
+echo Tail Won $tails times
